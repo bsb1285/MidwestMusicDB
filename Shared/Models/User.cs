99c837167e22ab30fe114daf22eecf563b26e212
Ben Baker <bsb1285@rit.edu>
2021-03-23 21:31:47 -0400

    Initial Commit

---
+++ Shared/Models/User.cs
@@ -0,0 +1,15 @@
+using System;
+namespace MidwestMusicDB.Shared.Models
+{
+    public class User
+    {
+        public string username { get; set; }
+        public string password { get; set; }
+        public string first_name {get; set; }
+        public string last_name {get; set; }
+        public DateTime last_access_date { get; set; }
+        public DateTime signup_date { get; set; }
+        public string email { get; set; }
+        
+    }
+}
