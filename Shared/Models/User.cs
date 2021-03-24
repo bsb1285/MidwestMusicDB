7eab1110cf26e3e2ca944622cc0d8de0da37f1f9
Ben Baker <bsb1285@rit.edu>
2021-03-24 09:12:19 -0400

    Updated User

--- Shared/Models/User.cs
+++ Shared/Models/User.cs
@@ -1,11 +1,12 @@
 using System;
 
 
+
+
 namespace MidwestMusicDB.Shared.Models
 {
     public class User
     {
-        []
         public string username { get; set; }
         public string password { get; set; }
         public string first_name {get; set; }
